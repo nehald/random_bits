8f08985a4d194cbaa1727079f65d93e01867c36f
Ubuntu <ubuntu@ip-10-197-34-227.us-west-1.compute.internal>
2013-09-18 07:14:07 +0000

    helloZ

--- index_test.py
+++ index_test.py
@@ -1,10 +1,35 @@
 from py2neo import * 
+import sys
+
+for k in range(100):
+    batch.get_or_create_indexed_node(index_name,'id',k,{'id':str(k+1.23)})
+batch.submit()
+
+def parse_dbpedia(fname,batch,index):
+	try:
+		f=open(fname)
+		f.readline()
+	except:
+		sys.exit(-1)
+        old_nodename = ""
+        node1=None
+        node2=None
+        for line in f:
+                fsplit =line.split(" ")
+                if len(fsplit) == 4:
+                        try:
+                                vals = [fsplit[i].split("/")[4][:-1] for i in range(0,3)]
+                       		if vals[0] != old_name:
+					batch.get_or_create_indexed_node(index,'id', 				 
+
+			except:
+                                print  'Unable to parse string'
+                                pass;
+
 
 graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
 index_name = "index2"
 idx=graph_db.get_or_create_index(neo4j.Node,index_name)
 batch = neo4j.WriteBatch(graph_db)  # batch is linked to graph database
 
-for k in range(100):
-    batch.get_or_create_indexed_node(index_name,'id',k,{'id':str(k+1.23)})
-batch.submit()
+parse_dbpedia("/mnt/test_output.dat",batch,idx)
--- parse_dbpedia_2.py
+++ parse_dbpedia_2.py
@@ -7,23 +7,21 @@ def parse_dbpedia(fname):
 	old_nodename = ""
 	node1=None
 	node2=None
-        try:
-		for line in f:	
-			fsplit =line.split(" ")
-			if len(fsplit) == 4:
-                		vals = [fsplit[i].split("/")[4][:-1] for i in range(0,3)]
+	for line in f:	
+		fsplit =line.split(" ")
+		if len(fsplit) == 4:
+               		try:	
+				vals = [fsplit[i].split("/")[4][:-1] for i in range(0,3)]
 				if vals[0] != old_nodename:
-					print "changing node from ",old_nodename+" to "+ vals[0]
 					node1 = g.vertices.create({'name':vals[0]})
 				node2 = g.vertices.create({'name':vals[2]})
-				#print vals[0]+"-----> "+vals[2] +"\n"
-				edge = g.edges.create(node1,"wikilinks",node2);
-				edge = g.edges.create(node2,"wikilinks",node1);
+				edge = g.edges.create(node1,vals[1],node2);
+				edge = g.edges.create(node2,vals[1],node1);
+				print vals[0]+"-----> "+vals[2] +"\n"
 				old_nodename = vals[0]
-        except:
-                print "Unable to parse string"
-		pass;	
-       
+			except:
+				print  'Unable to parse string'
+				pass; 
 ##
 # Bulbs 0.3 Neo4j Batch Example
 # by James Thornton (http://jamesthornton.com)
@@ -45,4 +43,4 @@ g = Graph()
 g.clear()
 ## down page_links_en.nt from dpedia site
 #parse_dbpedia("/mnt/page_links_en.nt")  
-parse_dbpedia("/tmp/test_output.nt")  
+parse_dbpedia("/tmp/output.dat")  
